236fa5087a5523f126d8717fe7a4192899828dab
Laykel <layke.luc@gmail.com>
2019-11-09 19:27:52 +0100

    Actually fixed data generation script

--- docker/mysql/getdata/users-visits.py
+++ docker/mysql/getdata/users-visits.py
@@ -6,6 +6,22 @@ user_output_file = "../db-data/4_insert-users.sql"
 visit_output_file = "../db-data/5_insert-visits.sql"
 nbr_of_users = 1000
 
+# Insert random number of visits
+def generate_visits(username):
+    query = ""
+
+    nbr_of_visits = randint(0, 10000)
+    for i in range(0, nbr_of_visits):
+        year = randint(1880, 2019)
+        month = randint(1, 12)
+        day = randint(1, 28)
+        date = "{:04}-{:02}-{:02}".format(year, month, day)
+        city = randint(1, 12900)
+
+        query +=  "('{0}', {1}, '{2}', '{2}'),\n".format(username, city, date)
+
+    return query
+
 # Create sql query and add base users
 user_query = "INSERT INTO User (username, passwordHash) VALUES\n"
 user_query += "('luc', '$2a$10$XEdw2VBLYtp72kXQ4tAXku3cVQLsg/06BdlXIy8oL90yueemZ5ZJu'),\n"
@@ -18,7 +34,11 @@ nameList = generate_username(nbr_of_users)
 # Make sure every username is unique
 names = list(dict.fromkeys(nameList))
 
+# Create queries for visits at the same time
 visit_query = "INSERT INTO Visit (fk_username, fk_idCity, startDate, endDate) VALUES\n"
+
+visit_query += generate_visits("luc")
+visit_query += generate_visits("alison")
 
 # Add names to sql query
 for name in names:
@@ -26,15 +46,8 @@ for name in names:
     hashed = hashpw(pwd.encode('utf-8'), gensalt())
     user_query += "('{0}', '{1}'),\n".format(name, hashed.decode('utf-8'))
 
-    nbr_of_visits_per_user = randint(0, 10000)
-    # Insert visits
-    for i in range(0, nbr_of_visits_per_user):
-        year = randint(1880, 2019)
-        month = randint(1, 12)
-        day = randint(1, 28)
-        date = "{:04}-{:02}-{:02}".format(year, month, day)
-        city = randint(1, 12900)
-        visit_query += "('{0}', {1}, '{2}', '{2}'),\n".format(name, city, date)
+    # Insert random number of visits
+    visit_query += generate_visits(name)
 
 # Remove trailing comma
 user_query = user_query[:len(user_query)-2]
@@ -42,8 +55,10 @@ user_query += ";\n"
 visit_query = visit_query[:len(visit_query)-2]
 visit_query += ";\n"
 
+# Write to files
 with open(user_output_file, "w+") as writer:
     writer.write(user_query)
 
 with open(visit_output_file, "w+") as writer:
     writer.write(visit_query)
+
