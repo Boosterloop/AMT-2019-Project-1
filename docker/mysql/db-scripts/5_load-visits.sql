2c68c4d50c2eb4f6537b6f5ee0fa7b38e3eb3062
Laykel <layke.luc@gmail.com>
2019-11-11 08:54:01 +0100

    Fix visits load script

--- docker/mysql/db-scripts/5_load-visits.sql
+++ docker/mysql/db-scripts/5_load-visits.sql
@@ -1,7 +1,11 @@
-LOAD DATA INFILE '/var/lib/mysql-files/visits.csv'
+SET foreign_key_checks = 0;
+
+LOAD DATA INFILE '/load-data/visits.csv'
 INTO TABLE citylogdb.Visit
 COLUMNS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (idVisit, fk_username, fk_idCity, startDate, endDate)
 SET idVisit = NULL;
+
+SET foreign_key_checks = 1;
